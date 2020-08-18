1dc2f13a52c0e408290fa931cc8cadb423280695
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-08-18 19:47:40 +0000

    Create graph.h

---
+++ ShortestPathBrtweenNodes/graph.h
@@ -0,0 +1,39 @@
+#include <vector>
+#include <map>
+#include <cmath>
+
+template <typename Vertex = int, typename Weight = double>
+   class graph
+   {
+   public:
+      typedef Vertex                     vertex_type;
+      typedef Weight                     weight_type;
+      typedef std::pair<Vertex, Weight>  neighbor_type;
+      typedef std::vector<neighbor_type> neighbor_list_type;
+   public:
+      void add_edge(Vertex const source, Vertex const target,
+                    Weight const weight, bool const bidirectional = true)
+      {
+         adjacency_list[source].push_back(std::make_pair(target, weight));
+         adjacency_list[target].push_back(std::make_pair(source, weight));
+}
+size_t vertex_count() const { return adjacency_list.size(); }
+      std::vector<Vertex> verteces() const
+      {
+         std::vector<Vertex> keys;
+         for (auto const & kvp : adjacency_list)
+            keys.push_back(kvp.first);
+         return keys;
+}
+      neighbor_list_type const & neighbors(Vertex const & v) const
+      {
+         auto pos = adjacency_list.find(v);
+         if (pos == adjacency_list.end())
+            throw std::runtime_error("vertex not found");
+         return pos->second;
+}
+      constexpr static Weight Infinity =
+                std::numeric_limits<Weight>::infinity();
+   private:
+      std::map<vertex_type, neighbor_list_type> adjacency_list;
+};
