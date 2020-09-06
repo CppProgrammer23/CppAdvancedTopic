b4a3679ac0d038b6760c2f941d2855dfa2668881
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-09-06 17:08:00 +0100

    Create main.cpp

---
+++ uVector/main.cpp
@@ -0,0 +1,46 @@
+#include "Vector_.h"
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const Vector_<T>& v)
+{
+	
+}
+
+
+int main()
+{
+	Vector_<int> myvec{ 1,8,6,7,3 };
+	//Vector_<int> vec = std::move(myvec); // move c-tor
+	Vector_<int> vec;
+	vec = myvec; // copy assignment 
+	vec.contains();
+	myvec.contains();
+	myvec.push_back_(12);
+	myvec.contains();
+	myvec.push_back_(15);
+	myvec.push_back_(941);
+	myvec.contains();
+	//myvec.clear_();
+	if (myvec.empty_() == true)
+		std::cout << "Empty!\n";
+	//myvec.contains();
+	std::cout << myvec.at_(2) << '\n';
+	std::cout << myvec.back_() << '\n';
+	std::cout << myvec.front_() << '\n';
+	myvec.sort_();
+	myvec.contains();
+	std::cout << std::boolalpha;
+	std::cout << myvec.find_(3) << '\n';
+	myvec.insert_(4, 25);
+	myvec.contains();
+	myvec.pop_back_();
+	myvec.contains();
+
+	Vector_<const char*> MyStr{ "Hello", "Ok" };
+	MyStr.contains();
+	MyStr.push_back_("Never mind");
+	MyStr.contains();
+	std::cout << MyStr.at_(2) << '\n';
+	std::cout << MyStr.find_("Ok") << '\n';
+	return 0;
+}
